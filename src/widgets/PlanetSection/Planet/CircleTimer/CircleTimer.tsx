e66e3142f64e7d04e137310a1419d44fe44c194f
Vladislav Okhotnikov <rettakecsgo@gmail.com>
2023-09-01 16:07:11 +0300

    -fix (new spinner + styled components)

---
+++ src/widgets/PlanetSection/Planet/CircleTimer/CircleTimer.tsx
@@ -0,0 +1,83 @@
+//@ts-nocheck
+import { ComponentProps, FunctionComponent } from "react";
+import styled, { keyframes } from "styled-components";
+
+interface ICircleTimerProps extends ComponentProps<"svg"> {
+  duration: number;
+  loop: boolean;
+  fillColor: string;
+  trackColor: string;
+  size: number;
+  strokeWidth: number;
+}
+
+const CircleTimer: FunctionComponent<ICircleTimerProps> = ({
+  duration,
+  fillColor,
+  loop,
+  size,
+  strokeWidth,
+  trackColor
+}) => {
+  const sizeWithStroke = size + strokeWidth;
+  const center = size / 2 + strokeWidth / 2;
+  const radius = size / 2;
+
+  const circleFill = ({ r }) => keyframes`
+  0% {
+    stroke-dashoffset: ${2 * Math.PI * r}};
+  }
+  100% {
+    stroke-dashoffset: 0;
+  }
+`;
+
+interface IAnimatedCircle {
+  circleFill: any;
+  duration: number;
+  loop: boolean;
+}
+  
+const AnimatedCircle = styled.circle<IAnimatedCircle>`
+  stroke-dasharray: ${({ r }) => 2 * Math.PI * r};
+  animation: ${circleFill} ${({ duration }) => `${duration}s`} linear;
+  animation-fill-mode: forwards;
+  animation-iteration-count: ${({ loop }) => (loop ? "infinite" : 1)};
+`;
+
+  return (
+    <svg
+      width={`${sizeWithStroke}px`}
+      height={`${sizeWithStroke}px`}
+      viewBox={`0 0 ${sizeWithStroke} ${sizeWithStroke}`}
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <g fill="none" stroke="none">
+        <circle
+          className="circle_track"
+          cx={center}
+          cy={center}
+          r={radius}
+          stroke={trackColor}
+          strokeWidth={strokeWidth}
+        />
+        <AnimatedCircle
+          className="circle_fill"
+          cx={center}
+          cy={center}
+          duration={duration}
+          loop={loop}
+          r={radius}
+          stroke={fillColor}
+          strokeWidth={strokeWidth}
+          transform={`rotate(-90, ${sizeWithStroke / 2}, ${sizeWithStroke /
+            2})`}
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default CircleTimer;
--- src/widgets/PlanetSection/Planet/Planet.tsx
+++ src/widgets/PlanetSection/Planet/Planet.tsx
@@ -1,56 +1,29 @@
-import { FunctionComponent, useEffect } from "react";
+import { FunctionComponent } from "react";
 import styles from "./Planet.module.css";
 import planet from "../../../assets/images/planet.png";
 import BlockTimer from "./CircleTimer/BlockTimer/BlockTimer";
+import CircleTimer from "./CircleTimer/CircleTimer";
 
 interface IProps {
   isMainPage: boolean;
 }
 
 const Planet: FunctionComponent<IProps> = ({isMainPage}) => {
-  useEffect(() => {
-    const circle: any = document.querySelector("#circle");
-    if (circle) {
-      const raduis = circle?.r.baseVal.value;
-      const circumference = 2 * Math.PI * raduis;
-      const timeValue = 750;
-      let i = 0;
-      const step: number = 400 / timeValue;
-
-      circle.style.strokeDasharray = `${circumference} ${circumference}`;
-      circle.style.strokeDashoffset = circumference;
-      const setProgressBar = (percent: number) => {
-        const offset = circumference - (percent / 100) * circumference;
-        circle.style.strokeDashoffset = offset;
-      };
-
-      const interval = setInterval(() => {
-        if (++i === step) {
-          clearInterval(interval);
-        } else {
-          setProgressBar(step * i);
-        }
-      }, 100);
-    }
-  }, []);
-
   return (
     <div className={styles.ellipse}>
       <div className={styles.blockWithTimer}>
         {isMainPage && <BlockTimer />}
       </div>
-      <svg width={"486"} className={styles.timer} height={"486"}>
-        <circle
-          id={"circle"}
-          width="1"
-          stroke={"#E75626"}
-          fill="transparent"
-          r="243"
-          cx="243"
-          cy="243"
+      <div className={styles.timer}>
+        <CircleTimer
+        duration={20}
+        loop={true}
+        strokeWidth={0.5}
+        size={486}
+        trackColor="transparent"
+        fillColor="#E75626"
         />
-      </svg>
-
+      </div>
       <div className={styles.ellipse__outlined}>
         <img className={styles.image} src={planet} alt="planet" />
       </div>
